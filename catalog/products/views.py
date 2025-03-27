21685fbc3f0000079ca8653c72d59219c9f26609
Bohdan Savin <bogdan07112009@gmail.com>
2025-03-27 15:33:54 +0200

    filters done

--- catalog/products/views.py
+++ catalog/products/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Product
+from .models import Product, Category
 
 
 # Create your views here.
+
+
 def index(request):
     products = Product.objects.all()
+    category_name = request.GET.get("category")
+    filter_name = request.GET.get("filter")
 
-    return render(request, "index.html", context={"products": products})
+    categories = Category.objects.all()
+
+    if category_name:
+        category = Category.objects.get(name=category_name)
+        products = products.filter(category=category)
+
+    match filter_name:
+        case "price_increase":
+            products = products.order_by("price")
+        case "price_decrease":
+            products = products.order_by("-price")
+        case "rating_increase":
+            products = products.order_by("rating")
+        case "rating_decrease":
+            products = products.order_by("-rating")
+
+    return render(
+        request, "index.html", context={"products": products, "categories": categories}
+    )
 
 
 def product_details(request, product_id):
